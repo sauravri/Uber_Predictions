87c2d0bb7b0448440e9c98fac48a7cea4dc25939
Saurav Ingle <43991154+sauravri@users.noreply.github.com>
2020-07-21 17:23:05 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -19,4 +19,4 @@ def predict():
     return render_template('index.html',prediction_text = 'Number of Weekly Rides should be : {}'.format(math.floor(output)))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
